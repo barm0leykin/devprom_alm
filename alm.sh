0c5333bcca63e4e8cc46c23106eec268ebcf58d6
Andrey Kudryashov <andreyka001@gmail.com>
2019-06-13 15:26:27 +0300

    rm alm.sh

--- alm.sh
+++
@@ -1,3 +0,0 @@
-docker run -d --name  alm --restart unless-stopped -p 8080:80 -p 3305:3306 --env MYSQL_ROOT_PASSWORD=devprom_pass --cap-add=SYS_ADMIN --cap-add=NET_ADMIN -v $(pwd)/backup:/var/www/devprom/backup -v /etc/localtime:/etc/localtime:ro devprom/alm:latest
-
-chown -R www-data:www-data ./
